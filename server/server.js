fbe67b05cf8cbacdc64135493dd1d0a0730035bd
lizJenkins1 <110464307+lizJenkins1@users.noreply.github.com>
2022-08-05 14:58:08 +0100

    Exercise 2 solution

--- server/server.js
+++ server/server.js
@@ -23,5 +23,45 @@ var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
 
+// GET
+app.get('/lions', (req, res) => {
+    console.log(req)
+    res.json(lions);
+});
+
+// FETCH
+app.get('/lions/:id', (req, res) => {
+    console.log(req.params);
+    var lion = _.find(lions, {id: req.params.id});
+    res.json(lion || {});
+});
+
+// POST
+app.post('/lions', (req, res) => {
+    var lion = req.body;
+    id++;
+    lion.id = id + '';
+
+    lions.push(lion);
+    
+    res.json(lion);
+});
+
+// PUT
+app.put('/lions/:id', function(req, res) {
+    var update = req.body;
+    if (update.id) {
+      delete update.id
+    }
+  
+    var lion = _.findIndex(lions, {id: req.params.id});
+    if (!lions[lion]) {
+      res.send();
+    } else {
+      var updatedLion = _.assign(lions[lion], update);
+      res.json(updatedLion);
+    }
+});
+
 app.listen(3000);
 console.log('on port 3000');
